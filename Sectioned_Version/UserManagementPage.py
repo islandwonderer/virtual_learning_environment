cfc1bfb35cf17bce75e0947eeee3508441e9aa5b
Gabriel Ruiz <eliduberry@gmail.com>
2020-02-16 19:11:20 +1300

    Fixed Suspend System and eliminated password from the update user options. Need rework on password system.

--- Sectioned_Version/UserManagementPage.py
+++ Sectioned_Version/UserManagementPage.py
@@ -42,23 +42,19 @@ class UserManagementPage(tk.Frame):
         email_label.grid(row=8, column=2, padx=10, sticky=tk.W)
         self.email_entry = tk.Entry(self, width=20, font="none 12 bold", highlightthickness=0)
         self.email_entry.grid(row=9, column=2, padx=10)
-        pass_label = tk.Label(self, text="Password")
-        pass_label.grid(row=10, column=2, padx=10, sticky=tk.W)
-        self.pass_entry = tk.Entry(self, width=20, font="none 12 bold", highlightthickness=0)
-        self.pass_entry.grid(row=11, column=2, padx=10)
-        suspend_label = tk.Label(self, text="Suspended")
-        suspend_label.grid(row=12, column=2, padx=10, sticky=tk.W)
+        suspend_label = tk.Label(self, text="Status")
+        suspend_label.grid(row=10, column=2, padx=10, sticky=tk.W)
         suspend_frame = tk.Frame(self)
-        suspend_frame.grid(row=13, column=2, sticky=tk.NSEW, padx=10)
+        suspend_frame.grid(row=11, column=2, sticky=tk.NSEW, padx=10)
         self.suspended = False
-        self.yes_button = tk.Button(suspend_frame, command=lambda: self.toggle_suspend(),
-                                    text="Yes", state=(tk.ACTIVE if self.suspended is False else tk.DISABLED),
+        self.suspend_button = tk.Button(suspend_frame, command=lambda: self.toggle_suspend(),
+                                    text="Suspend", state=tk.DISABLED,
                                     highlightbackground="#ffffff")
-        self.no_button = tk.Button(suspend_frame, command=lambda: self.toggle_suspend(),
-                                   text="No", state=(tk.DISABLED if self.suspended is False else tk.ACTIVE),
-                                   highlightbackground="#ffffff")
-        self.no_button.pack(side=tk.RIGHT)
-        self.yes_button.pack(side=tk.RIGHT)
+        self.suspend_button.pack(side=tk.LEFT)
+        pass_label = tk.Label(self, text="Password")
+        pass_label.grid(row=12, column=2, padx=10, sticky=tk.W)
+        self.pass_entry = tk.Entry(self, width=20, font="none 12 bold", highlightthickness=0)
+        self.pass_entry.grid(row=13, column=2, padx=10)
         self.web_site = tk.Label(self, text="Click Here To Visit Student Site", fg="blue", cursor="hand2")
         self.web_site.grid(row=14, column=2, sticky=tk.NSEW, padx=10, pady=10)
         self.web_site.bind("<Button-1>", lambda e: self.open_site())
@@ -72,16 +68,16 @@ class UserManagementPage(tk.Frame):
     def toggle_suspend(self):
         if self.suspended is False:
             self.suspended = True
-            self.yes_button.config(state=tk.ACTIVE)
-            self.no_button.config(state=tk.DISABLED)
-            self.yes_button.update()
-            self.no_button.update()
+            self.suspend_button.config(text="Enable")
+            self.suspend_button.update()
+            self.selected_user.isSuspended = False
+            gt.save_user(self.selected_user)
         else:
             self.suspended = False
-            self.yes_button.config(state=tk.DISABLED)
-            self.no_button.config(state=tk.ACTIVE)
-            self.yes_button.update()
-            self.no_button.update()
+            self.suspend_button.config(text="Suspend")
+            self.suspend_button.update()
+            self.selected_user.isSuspended = True
+            gt.save_user(self.selected_user)
 
     def on_select(self, event):
         widget = event.widget
@@ -94,6 +90,9 @@ class UserManagementPage(tk.Frame):
         self.email_entry.insert(0, self.selected_user.eMail)
         self.pass_entry.insert(0, self.selected_user.password)
         self.suspended = self.selected_user.isSuspended
+        if self.suspended:
+            self.suspend_button.config(text="Unsuspend")
+        self.suspend_button.config(state=tk.ACTIVE)
         self.upd_button.config(state=tk.ACTIVE)
         self.del_button.config(state=tk.ACTIVE)
 
@@ -101,8 +100,8 @@ class UserManagementPage(tk.Frame):
         global vm
         messagebox.showinfo("Warning", "This process may take while. A window will open on your browser when its ready.", parent=self)
         curr_user = self.selected_user
-        if curr_user.assignedVM is not None:
-            vm = gt.get_vm_object(curr_user.assignedVM)
+        if curr_user.assigned_VM is not None:
+            vm = gt.get_vm_object(curr_user.assigned_VM)
             vm.startInstance()
             vm.isInstanceReady()
             site = "http://" + vm.getInstaceIP() + "/moodle"
@@ -117,8 +116,6 @@ class UserManagementPage(tk.Frame):
         curr_user.firstName = self.fn_entry.get()
         curr_user.lastName = self.ln_entry.get()
         curr_user.eMail = self.email_entry.get()
-        curr_user.setCustPassword(self.pass_entry.get())
-        curr_user.isSuspended = self.suspended
         gt.save_user(curr_user)
         messagebox.showinfo("Update", "The user information has been updated.", parent=self)
 
@@ -142,7 +139,6 @@ class UserManagementPage(tk.Frame):
 
         else:
             messagebox.showinfo("Warning", "This is a teacher and cannot be removed this way.", parent=self)
-
         self.clear_form()
 
     def update_list(self):
@@ -153,6 +149,9 @@ class UserManagementPage(tk.Frame):
             text = text.format(user.lastName, user.firstName, user.studentID)
             self.user_list.insert(tk.END, text)
         self.user_list.update()
+
+    def update_password(self):
+        self.selected_user.setCustPassword(self.pass_entry.get())
 
     def clear_form(self):
         self.id_entry.delete(0, tk.END)
