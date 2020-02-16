49c5791d123c73db307dd63a4945ce90781d2316
Gabriel Ruiz <eliduberry@gmail.com>
2020-02-16 19:48:42 +1300

    User Management Page completed and tested

--- Sectioned_Version/UserManagementPage.py
+++ Sectioned_Version/UserManagementPage.py
@@ -42,19 +42,19 @@ class UserManagementPage(tk.Frame):
         email_label.grid(row=8, column=2, padx=10, sticky=tk.W)
         self.email_entry = tk.Entry(self, width=20, font="none 12 bold", highlightthickness=0)
         self.email_entry.grid(row=9, column=2, padx=10)
+        pass_label = tk.Label(self, text="Password")
+        pass_label.grid(row=10, column=2, padx=10, sticky=tk.W)
+        self.pass_entry = tk.Entry(self, width=20, font="none 12 bold", highlightthickness=0)
+        self.pass_entry.grid(row=11, column=2, padx=10)
         suspend_label = tk.Label(self, text="Status")
-        suspend_label.grid(row=10, column=2, padx=10, sticky=tk.W)
+        suspend_label.grid(row=12, column=2, padx=10, sticky=tk.W)
         suspend_frame = tk.Frame(self)
-        suspend_frame.grid(row=11, column=2, sticky=tk.NSEW, padx=10)
+        suspend_frame.grid(row=13, column=2, sticky=tk.NSEW, padx=10)
         self.suspended = False
         self.suspend_button = tk.Button(suspend_frame, command=lambda: self.toggle_suspend(),
                                     text="Suspend", state=tk.DISABLED,
                                     highlightbackground="#ffffff")
         self.suspend_button.pack(side=tk.LEFT)
-        pass_label = tk.Label(self, text="Password")
-        pass_label.grid(row=12, column=2, padx=10, sticky=tk.W)
-        self.pass_entry = tk.Entry(self, width=20, font="none 12 bold", highlightthickness=0)
-        self.pass_entry.grid(row=13, column=2, padx=10)
         self.web_site = tk.Label(self, text="Click Here To Visit Student Site", fg="blue", cursor="hand2")
         self.web_site.grid(row=14, column=2, sticky=tk.NSEW, padx=10, pady=10)
         self.web_site.bind("<Button-1>", lambda e: self.open_site())
@@ -88,7 +88,6 @@ class UserManagementPage(tk.Frame):
         self.fn_entry.insert(0, self.selected_user.firstName)
         self.ln_entry.insert(0, self.selected_user.lastName)
         self.email_entry.insert(0, self.selected_user.eMail)
-        self.pass_entry.insert(0, self.selected_user.password)
         self.suspended = self.selected_user.isSuspended
         if self.suspended:
             self.suspend_button.config(text="Enable")
@@ -98,7 +97,7 @@ class UserManagementPage(tk.Frame):
 
     def open_site(self):
         global vm
-        messagebox.showinfo("Warning", "This process may take while. A window will open on your browser when its ready.", parent=self)
+        messagebox.showinfo("Warning", "This process may take a while. A window will open on your browser when it's ready.", parent=self)
         curr_user = self.selected_user
         if curr_user.assigned_VM is not None:
             vm = gt.get_vm_object(curr_user.assigned_VM)
@@ -107,7 +106,7 @@ class UserManagementPage(tk.Frame):
             site = "http://" + vm.getInstaceIP() + "/moodle"
             webbrowser.open(site)
         else:
-            messagebox.showinfo("Warning", "This is a teacher and does not have VM assigned.", parent=self)
+            messagebox.showinfo("Warning", "This is a teacher and does not have a VM assigned.", parent=self)
 
     def update_user(self):
         curr_user = self.selected_user
@@ -116,7 +115,10 @@ class UserManagementPage(tk.Frame):
         curr_user.firstName = self.fn_entry.get()
         curr_user.lastName = self.ln_entry.get()
         curr_user.eMail = self.email_entry.get()
+        if self.pass_entry.get():
+            curr_user.setCustPassword(self.pass_entry.get())
         gt.save_user(curr_user)
+        self.update_list()
         messagebox.showinfo("Update", "The user information has been updated.", parent=self)
 
     def delete_user(self):
@@ -149,9 +151,6 @@ class UserManagementPage(tk.Frame):
             text = text.format(user.lastName, user.firstName, user.studentID)
             self.user_list.insert(tk.END, text)
         self.user_list.update()
-
-    def update_password(self):
-        self.selected_user.setCustPassword(self.pass_entry.get())
 
     def clear_form(self):
         self.id_entry.delete(0, tk.END)
